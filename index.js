53e6ba416fdce718210dad050f0e410f88c4a427
yonmetocodesoi <yonmeto@local>
2026-02-15 20:13:39 -0300

    Add cheerio dependency and global import

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const axios = require('axios');
 const cors = require('cors');
+const cheerio = require('cheerio');
 
 const app = express();
 const PORT = process.env.PORT || 1010;
